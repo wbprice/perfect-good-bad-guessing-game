be9ceb4b34857c7d8d957ce7c36ca103efbb53c6
Blaine Price <1wbprice@gmail.com>
2019-02-27 20:38:39 -0500

    Take in input, ask for a guess, generate a secret number

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use structopt::StructOpt;
+use read_input::prelude::*;
+use rand::Rng;
 
-#[derive(StructOpt)]
+#[derive(Debug, StructOpt)]
 struct Cli {
     #[structopt(short = "d", long = "digit")]
     digit: i8
@@ -8,4 +10,19 @@ struct Cli {
 
 fn main() {
     let args = Cli::from_args();
+    dbg!(args.digit);
+
+    // to implement logic for figuring out how many digits are in the guess number
+
+    let guess : i64 = input().msg("What is your guess?").get();
+    let secret_number_min = 100;
+    let secret_number_max = 999;
+    let secret_number = rand::thread_rng()
+        .gen_range(secret_number_min, secret_number_max);
+
+    dbg!(guess);
+    dbg!(secret_number);    
+
+    
 }
+
